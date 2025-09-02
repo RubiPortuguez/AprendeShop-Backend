d3b5d6230e681e9d88afb9275622b6f06ca3d82c
Carolina Ortiz <carolinaortizromero@gmail.com>
2025-09-02 11:53:46 -0600

    Se agregó el proyecto base de Spring Boot inicializado con Gradle para AprendeShop

---
+++ Backend/aprende-shop/src/main/java/org/aprende_shop/aprende_shop/AprendeShopApplication.java
@@ -0,0 +1,13 @@
+package org.aprende_shop.aprende_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendeShopApplication.class, args);
+	}
+
+}
