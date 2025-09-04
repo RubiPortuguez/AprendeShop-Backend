cc519c35570ea257e4886dd9e685993e52db92d1
Erikaahide <eahidehernandez@gmail.com>
2025-09-04 14:56:29 -0600

    compraUsuario

--- Backend/aprende-shop/src/main/java/org/aprende_shop/aprende_shop/model/CompraUsuario.java
+++ Backend/aprende-shop/src/main/java/org/aprende_shop/aprende_shop/model/CompraUsuario.java
@@ -1,52 +1,64 @@
 package org.aprende_shop.aprende_shop.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "compraUsuario")
 public class CompraUsuario {
-	private int fk_idUsuario;
-	private int fk_idCurso;
-	private int idCompra;
-	
-	private static long total=0;
-	
-	//constructor vacio
-    public CompraUsuario() {
-    	CompraUsuario.total++;
-		this.idCompra= (int) CompraUsuario.total;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idcompra")
+    private Integer idCompra;
+
+    @Column(name = "fkid_usuario", nullable = false)
+    private Integer fkIdUsuario;
+
+    @Column(name = "fkidcurso", nullable = false)
+    private Integer fkIdCurso;
+
+    // Constructors
+    public CompraUsuario() {}
+
+    public CompraUsuario(Integer fkIdUsuario, Integer fkIdCurso) {
+        this.fkIdUsuario = fkIdUsuario;
+        this.fkIdCurso = fkIdCurso;
     }
 
-    //constructor sin id
-    public CompraUsuario(int fk_idUsuario, int fk_idCurso) {
-		super();
-		this.fk_idUsuario = fk_idUsuario;
-		this.fk_idCurso = fk_idCurso;
-		CompraUsuario.total++;
-		this.idCompra= (int) CompraUsuario.total;
+	public Integer getIdCompra() {
+		return idCompra;
 	}
 
+	public void setIdCompra(Integer idCompra) {
+		this.idCompra = idCompra;
+	}
 
-    public Integer getIdCompra() {
-        return idCompra;
-    }
+	public Integer getFkIdCurso() {
+		return fkIdCurso;
+	}
 
-    public Integer getFk_idUsuario() {
-        return fk_idUsuario;
-    }
+	public void setFkIdCurso(Integer fkIdCurso) {
+		this.fkIdCurso = fkIdCurso;
+	}
 
-    public void setFk_idUsuario(Integer fk_idUsuario) {
-        this.fk_idUsuario = fk_idUsuario;
-    }
-
-    public Integer getFk_idCurso() {
-        return fk_idCurso;
-    }
-
-    public void setFk_idCurso(Integer fk_idCurso) {
-        this.fk_idCurso = fk_idCurso;
-    }
+	public Integer getFkIdUsuario() {
+		return fkIdUsuario;
+	}
 
 	@Override
 	public String toString() {
-		return "CompraUsuario [fk_idUsuario=" + fk_idUsuario + ", fk_idCurso=" + fk_idCurso + ", idCompra=" + idCompra
+		return "CompraUsuario [idCompra=" + idCompra + ", fkIdUsuario=" + fkIdUsuario + ", fkIdCurso=" + fkIdCurso
 				+ "]";
 	}
+
+
+  
     
 }
